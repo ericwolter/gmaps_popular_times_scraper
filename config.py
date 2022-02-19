035c28efd36d64dd0b6af274096e83ef18052357
Eric <eric.wolter@gmx.de>
2022-02-19 09:14:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ HEADER_COLUMNS = ('place', 'url', 'scrape_time', 'day_of_week', 'hour_of_day', '
 
 # path to chrome and chromedriver
 CHROME_BINARY_LOCATION = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-CHROMEDRIVER_BINARY_LOCATION = '/usr/local/bin/chromedriver'
+CHROMEDRIVER_BINARY_LOCATION = '/opt/homebrew/bin/chromedriver'
 
 # keep an cache of the source htmls, with a timestamp in the filename
 # if so, they should be cleaned out once in a while, since they are 1MB each
@@ -17,7 +17,7 @@ SAVE_HTML = False
 
 # put your url or path here to a csv where the first column is a google maps url
 # google sheets - export as csv https://stackoverflow.com/a/33727897/2327328
-URL_PATH_INPUT = None
+URL_PATH_INPUT = 'test_urls.csv'
 #URL_PATH_INPUT = 'urls.csv'
 
 DEBUG = False
